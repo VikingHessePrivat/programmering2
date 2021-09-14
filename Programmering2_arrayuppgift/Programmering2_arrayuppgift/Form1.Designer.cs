e2775054ffa785e139571296259433024ee20b93
VikingHessePrivat <74158135+VikingHessePrivat@users.noreply.github.com>
2021-09-14 03:25:05 +0200

    Add files via upload

---
+++ Programmering2_arrayuppgift/Programmering2_arrayuppgift/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace Programmering2_arrayuppgift
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.inputSiffror = new System.Windows.Forms.RichTextBox();
+            this.inputTarget = new System.Windows.Forms.RichTextBox();
+            this.outputSvar = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(51, 9);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(713, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vilka två siffror blir tillsammans en annan?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(51, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Siffror: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(49, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 37);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Target: ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(379, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Beräkna";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // inputSiffror
+            // 
+            this.inputSiffror.Location = new System.Drawing.Point(158, 129);
+            this.inputSiffror.Name = "inputSiffror";
+            this.inputSiffror.Size = new System.Drawing.Size(170, 26);
+            this.inputSiffror.TabIndex = 4;
+            this.inputSiffror.Text = "";
+            // 
+            // inputTarget
+            // 
+            this.inputTarget.Location = new System.Drawing.Point(158, 217);
+            this.inputTarget.Name = "inputTarget";
+            this.inputTarget.Size = new System.Drawing.Size(170, 26);
+            this.inputTarget.TabIndex = 5;
+            this.inputTarget.Text = "";
+            // 
+            // outputSvar
+            // 
+            this.outputSvar.Location = new System.Drawing.Point(158, 294);
+            this.outputSvar.Name = "outputSvar";
+            this.outputSvar.Size = new System.Drawing.Size(249, 72);
+            this.outputSvar.TabIndex = 6;
+            this.outputSvar.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(348, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "(separera med mellanslag)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(82, 316);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 30);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Svar:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.outputSvar);
+            this.Controls.Add(this.inputTarget);
+            this.Controls.Add(this.inputSiffror);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox inputSiffror;
+        private System.Windows.Forms.RichTextBox inputTarget;
+        private System.Windows.Forms.RichTextBox outputSvar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Programmering2_arrayuppgift/Programmering2_arrayuppgift/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Programmering2_arrayuppgift
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                try { 
+                    string x = inputSiffror.Text;
+                    string[] stringArray = x.Split(' ');
+                    int[] number = Array.ConvertAll(stringArray, int.Parse);
+                    int target = Convert.ToInt32(inputTarget.Text);
+                    bool svar = false;
+
+
+                    for (int i = 0; i < number.Length; i++)
+                    {
+
+                        for (int j = 0; j < number.Length; j++)
+                        {
+
+                            if (number[i] + number[j] == target)
+                            {
+                                outputSvar.Text = $"{number[i]} och {number[j]}";
+                                svar = true;
+                            }
+
+                        }
+
+                        if (svar == true)
+                        {
+                            break;
+                        }
+                    }
+
+                    //om man ändrar siffrorna och det inte finns två som tillsammans blir target
+                    if (svar == false)
+                    {
+                        outputSvar.Text = $"Finns inte två siffror som tillsammans blir {target}";
+                    }
+                }
+                catch (System.FormatException)
+                {
+                    MessageBox.Show("Ange siffror i nummerform", "Fel", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
